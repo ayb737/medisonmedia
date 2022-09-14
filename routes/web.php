60f2c32847e7b87203cf067361f444ed6715053d
ayb737 <84677758+ayb737@users.noreply.github.com>
2022-09-14 15:06:02 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    if (Auth::check())
+    {
+        return view('dashboard');
+    } else {
+        return view('auth/register');
+    }
 });
 
 Route::get('/dashboard', function () {
