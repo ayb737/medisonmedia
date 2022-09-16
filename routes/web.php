ee503b550d5b4426efc2ec8d4b20dfe62bb37aa9
ayb737 <84677758+ayb737@users.noreply.github.com>
2022-09-16 14:11:10 +0300

    option 2 for crud commented out in web.php

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,44 @@ Route::post('countries', [C\CountryController::class, 'store']);
 
 Route::get('/country', [C\CountryController::class, 'index']);
 require __DIR__.'/auth.php';
+
+
+// option 2 of crud (without front):
+
+// Route::get('/create/{userID}', function($userID){
+//     $user = User::find($userID);
+//     $user->countries()->save(new Country(['name'=> 'Israel', 'ISO'=> 'IL']));
+//    });
+
+// Route::get('/read', function(){
+//     $user = User::findOrFail(1);
+//     foreach ($user->countries as $country) {
+//         echo $user->countries;
+//     }
+// });
+
+// Route::get('/update', function(){
+//     $user = User::findOrFail(1);
+//     if ($user->has('countries')) {
+//         foreach ($user->countries as $country) {
+//             if ($country->name == 'Israel') {
+//                 $country->name = 'Thailand';
+//                 $country->save();
+//             }
+//         }
+//     }
+// });
+
+// Route::get('/delete', function(){
+//     $user = User::findOrFail(1);
+//     foreach ($user->countries as $country) {
+//         $country->whereId(2)->delete();
+//     }
+// });
+
+//Attaches a country to a user, and will create another record
+// Route::get('/attach', function(){
+//     $user = User::findOrFail(2);
+//     $user->countries()->attach(2);
+// });
+
